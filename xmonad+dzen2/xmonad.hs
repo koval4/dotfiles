146a5197104d52a20abf0f3996b53152cb483fed
Roman Kovalivskiy <koval304@gmail.com>
2017-06-03 10:32:39 +0300

    - Updated xmonad config
    - Removed redundant imports
    
    + Added shortcuts for running most used programs
    
    * Fixed startupHook
    
    - Decreased panle font size

--- xmonad+dzen2/xmonad.hs
+++ xmonad+dzen2/xmonad.hs
@@ -1,60 +1,28 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 import XMonad
--- Prompt
-import XMonad.Prompt
-import XMonad.Prompt.RunOrRaise (runOrRaisePrompt)
-import XMonad.Prompt.AppendFile (appendFilePrompt)
-import XMonad.Util.EZConfig(additionalKeys)
-import XMonad.Util.SpawnOnce
-import XMonad.Operations
 
 import System.IO
-import System.Exit
 import Data.Monoid
+import qualified Data.Map as M
 
 import XMonad.Util.Run
 import XMonad.Util.Loggers
+import XMonad.Util.Timer
+import qualified XMonad.Util.ExtensibleState as XS
 
-import XMonad.Actions.CycleWS
-import XMonad.Actions.MouseResize
-
--- Hooks
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
-import XMonad.Hooks.SetWMName
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.UrgencyHook
-import XMonad.Hooks.FadeInactive
-import XMonad.Hooks.EwmhDesktops
 
---Layouts
 import XMonad.Layout.Fullscreen
 import XMonad.Layout.NoBorders
-import XMonad.Layout.Gaps
-import XMonad.Layout.PerWorkspace (onWorkspace, onWorkspaces)
-import XMonad.Layout.IM
-import XMonad.Layout.SimpleFloat
-import XMonad.Layout.Renamed (renamed, Rename(CutWordsLeft, Replace))
-import XMonad.Layout.Spacing
-import XMonad.Layout.ResizableTile
-import XMonad.Layout.LayoutHints
-import XMonad.Layout.LayoutModifier
-import XMonad.Layout.Grid
-import XMonad.Layout.Minimize
-import XMonad.Layout.Maximize
-import XMonad.Layout.BoringWindows (boringWindows)
-import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
-import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
-import XMonad.Layout.Reflect (reflectVert, reflectHoriz, REFLECTX(..), REFLECTY(..))
-import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), Toggle(..), (??))
-import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
-import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
-import XMonad.Util.Scratchpad (scratchpadSpawnAction, scratchpadManageHook, scratchpadFilterOutWorkspace)
-import qualified XMonad.Util.ExtensibleState as XS
-import XMonad.Util.Timer
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.StackTile
+import XMonad.Layout.PerWorkspace (onWorkspace)
+
+import XMonad.Actions.WindowGo
 
 import qualified XMonad.StackSet as W
-import qualified Data.Map as M
 
 data TIDState = TID TimerId deriving Typeable
 
@@ -64,7 +32,6 @@ instance ExtensionClass TIDState where
 main :: IO ()
 main = do
         dzenBar <- spawnPipe myBar
-        --infoPanel <- spawnPipe "/home/koval4/.xmonad/lemonbar_config.sh"
         xmonad $ fullscreenSupport $ def {
           terminal             = "termite"
         , workspaces           = myWorkspaces
@@ -82,11 +49,10 @@ main = do
 
 myStartup :: X ()
 myStartup = do
-    spawnOnce "feh --bg-scale --randomize /home/koval4/Pictures/ene.png"
-    spawnOnce "mpd"
-    spawnOnce "mpc update"
-    spawnOnce "twmnd"
-    --spawnOnce "/home/koval4/scripts/tasker startReminder /home/koval4/todo.txt"
+    spawn "feh --bg-scale --randomize /home/koval4/Pictures/ene.png &"
+    spawn "mpd &"
+    spawn "mpc update &"
+    spawn "twmnd &"
     startTimer 1 >>= XS.put . TID
 
 clockEventHook :: Event -> X All
@@ -108,22 +74,14 @@ term = "\xf120"
 media = "\xf008"
 other = "\xf067"
 
-myBar = "lemonbar -p -d -g 1440x25+0+0 " ++ myBarStyle ++ " | zsh"
+myBar = "lemonbar -p -d -g 1440x22+0+0 " ++ myBarStyle ++ " | zsh"
 myBarStyle = " -F \"#e6f7ff\" -B \"#0b0b14\" " ++ myBarFont
-myBarFont = " -f \"M+1mn:size=10\" -f \"FontAwesome:size=10\" "
+myBarFont = " -f \"M+1mn:size=9\" -f \"FontAwesome:size=9\" "
 
 myLayoutHook = avoidStruts
-               -- $ mouseResize
-               -- $ windowArrange
-               -- $ mkToggle (NBFULL ?? NOBORDERS ?? EOT)
-               -- $ renamed [CutWordsLeft 4]
-               -- $ maximize
-               -- $ minimize
-               -- $ boringWindows
-               $ smartBorders
                $ onWorkspace web webTiled
                $ onWorkspace media mediaTiled
-               $ tiled ||| Full
+               $ emptyBSP ||| Full
                where tiled = Tall nmaster delta ratio
                      mediaTiled = noBorders $ tiled
                      webTiled = Tall nmaster delta webRatio
@@ -149,7 +107,7 @@ myManageHook = (composeAll . concat $
         -- classnames
         myFloats  = ["VirtualBox","Xmessage","XFontSel","Downloads","feh","Pidgin"]
         myOffice  = ["libreoffice", "libreoffice-startcenter", "libreoffice-writer", "libreoffice-impress", "libreoffice-calc", "libreoffice-draw"]
-        myWebs    = ["vivaldi-snapshot", "Firefox", "Google-chrome", "Chromium", "Chromium-browser","Pidgin","Slack","telegram-desktop"]
+        myWebs    = ["vivaldi-snapshot", "Firefox", "Google-chrome", "Chromium", "Chromium-browser","Pidgin","Slack","TelegramDesktop"]
         myMedia   = ["rhythmbox", "Vlc", "baka-mplayer"]
         myDev     = ["QtCreator", "codeblocks", "MonoDevelop", "Emacs"]
         myBack    = ["Transmission-gtk"]
@@ -189,13 +147,16 @@ myKeys x  = M.union (M.fromList (newKeys x)) (keys def x)
 -- Add new and/or redefine key bindings
 newKeys conf@XConfig {XMonad.modMask = modm} =
     [ ((0         , xK_Print  ), spawn "spectacle")
+    , ((mod4Mask  , xK_F2     ), spawn "rofi -show run")
     , ((mod1Mask  , xK_Shift_L), spawn "/home/koval4/scripts/layout_switch.sh")
     , ((shiftMask , xK_Alt_L  ), spawn "/home/koval4/scripts/layout_switch.sh")
-    , ((mod1Mask  , xK_j      ), spawn "setxkbmap jp")
     , ((mod4Mask  , xK_f      ), spawn "firefox")
-    , ((mod4Mask  , xK_q      ), spawn "xmonad --recompile && (pkill lemonbar; xmonad --restart)")
+    , ((mod4Mask  , xK_e      ), spawn "emacs")
+    , ((mod4Mask  , xK_b      ), spawn "baka-mplayer")
+    , ((mod4Mask  , xK_g      ), spawn "telegram-desktop")
+    , ((mod4Mask  , xK_r      ), raiseMaybe (runInTerm "" "ranger")  (resource =? "ranger"))
+    , ((mod4Mask  , xK_m      ), raiseMaybe (runInTerm "" "ncmpcpp") (resource =? "ranger"))
     , ((mod1Mask  , xK_Tab    ), windows W.focusUp >> windows W.shiftMaster)
-    , ((mod4Mask  , xK_F2     ), spawn "rofi -show run")
     , ((mod4Mask  , xK_s      ), sendMessage ToggleStruts)
     , ((0         , 0x1008ff13), spawn "amixer set Master 2dB+")
     , ((0         , 0x1008ff11), spawn "amixer set Master 2dB-")
